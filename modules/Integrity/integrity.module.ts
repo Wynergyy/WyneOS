cf9246d49e0d274e51c5fcf7a9d5091a8f9825fd
WFSL <95259192+Wynergyy@users.noreply.github.com>
2025-12-03 09:12:23 +0000

    Phase 1 Module Framework initialised

---
+++ modules/Integrity/integrity.module.ts
@@ -0,0 +1,10 @@
+﻿import { ModuleLoader } from "../module-loader"
+
+export const IntegrityModule = {
+  name: "IntegrityModule",
+  initialise() {
+    console.log("[IntegrityModule] Phase 1 integrity systems online.")
+  }
+}
+
+ModuleLoader.register(IntegrityModule)
---
+++ modules/module-loader.ts
@@ -0,0 +1,19 @@
+﻿export interface WyneModule {
+  name: string
+  initialise: () => void
+}
+
+export class ModuleLoader {
+  private static modules: WyneModule[] = []
+
+  static register(module: WyneModule) {
+    this.modules.push(module)
+    console.log(\[ModuleLoader] Registered module: \\)
+  }
+
+  static initialiseAll() {
+    console.log("[ModuleLoader] Initialising Phase 1 modules…")
+    this.modules.forEach(m => m.initialise())
+    console.log("[ModuleLoader] All modules initialised.")
+  }
+}
